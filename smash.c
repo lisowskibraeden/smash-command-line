05267095c6ed2d2a643a1ddd117099f700b0fb91
lisowskibraeden <lisowskibraeden@gmail.com>
2020-02-09 20:30:17 -0600

    runs ls & ls & ls...

--- smash.c
+++ smash.c
@@ -5,10 +5,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void parsecommand(char** command, int size_command, char** path, int size_path) {
-    printf("ehlo\n");
-}
-
 void runcommand(char** command, int size_command, char** path, int size_path) {
     char check[1024];
     for (int i = 0; i < size_path; i++) {
@@ -17,20 +13,36 @@ void runcommand(char** command, int size_command, char** path, int size_path) {
         //check if executable can run
         if (access(check, X_OK) == 0) {
             //run the command
-            int pid = fork();
-            if (pid == 0) {
-                //child runs
-                char* cla[255];
-                memcpy(cla, command, 256);
-                execv(check, cla);
-            } else {
-                //parent waits until done
-                wait(NULL);
-            }
+            execv(check, command);
         } else {
             char error_message[30] = "An error has occurred\n";
             write(STDERR_FILENO, error_message, strlen(error_message));
         }
+    }
+}
+
+void parsecommand(char** command, int size_command, char** path, int size_path) {
+    char** cla = malloc(sizeof(char*) * 256);
+    int i = 0;
+    int children = 0;
+    int size = 0;
+    while (i < size_command) {
+        size = 0;
+        while (i < size_command && strcmp(command[i], "&") != 0) {
+            cla[i] = command[i];
+            size++;
+            i++;
+        }
+        int pid = fork();
+        if (pid == 0) {
+            runcommand(cla, size, path, size_path);
+        }else{
+            children++;
+        }
+        i++;
+    }
+    for(int x = 0; x < children; x++){
+        wait(NULL);
     }
 }
 
@@ -48,6 +60,11 @@ void mainloop(FILE* file) {
         int count = -1;
         //seperate out each CLA
         while ((out[count + 1] = strtok_r(input, " ", &input)) != NULL) {
+            char* tabs;
+            while ((tabs = strstr(out[count + 1], "\t")) != NULL) {
+                int index = tabs - out[count + 1];
+                memmove(&out[count + 1][index], &out[count + 1][index] + 1, strlen(out[count + 1]) - index);
+            }
             count++;
         }
         //if just an endline rerun loop
@@ -111,35 +128,21 @@ void mainloop(FILE* file) {
                     if (strcmp(out[i], "") != 0 && out[i][0] != '\0') {
                         char* found;
                         char* lastfound;
-                        if ((found = strstr(out[i], "&")) != NULL) {
-                            do {
+                        if ((found = strstr(out[i], "&")) != NULL && strcmp(out[i], "&") != 0) {
+                            while ((found = strtok_r(found, "&", &found)) != NULL) {
                                 command[size_command] = malloc(1 + found - out[i]);
-                                strncpy(command[size_command], out[i], found - out[i]);
+                                strncpy(command[size_command], out[i], found - out[i] - 1);
                                 command[size_command][found - out[i]] = '\0';
-                                if(strcmp(command[size_command], "") == 0){
-                                    free(command[size_command]);
-                                    size_command--;
-                                }
                                 size_command++;
-                                command[size_command] = malloc(1);
-                                strncpy(command[size_command], found, 1);
+                                command[size_command] = "&";
                                 size_command++;
                                 found++;
-                                lastfound = found;
-                            } while ((found = strstr(found, "&")) != NULL);
-                            if (strcmp(lastfound, "") != 0) {
-                                command[size_command] = lastfound;
-                                size_command++;
                             }
                         } else {
                             command[size_command] = out[i];
                             size_command++;
                         }
                     }
-                }
-                printf("print\n");
-                for (int i = 0; i < size_command; i++) {
-                    printf("index: %d string: %s\n", i, command[i]);
                 }
                 parsecommand(command, size_command, path, size_path);
             }
